3930a7befd3197d317305389d89b6cd03c814163
tushaaaaar <tushars202003@gmail.com>
2025-01-27 15:17:33 +0530

    uploading the project

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ try:
 except:
     print("Training new model...")
     mnist = tf.keras.datasets.mnist
-    x_train, y_train, x_test, y_test = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     x_train = tf.keras.utils.normalize(x_train, axis=1)
     x_test = tf.keras.utils.normalize(x_test, axis=1)
@@ -21,13 +21,16 @@ except:
     model.add(tf.keras.layers.Dense(128, activation='relu'))
     model.add(tf.keras.layers.Dense(128, activation='relu'))
     model.add(tf.keras.layers.Dense(10, activation='softmax'))
-    
+        
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.fit(x_train, y_train, epochs=3)
     model.save('handwritten.keras')
     print("Model trained and saved successfully!")
 
+
+    model = tf.keras.models.load_model('handwritten.keras')
+    print("Model loaded successfully!")
 
 # Function to predict the digit
 def predict_digit(image):
